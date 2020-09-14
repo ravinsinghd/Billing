1d51c5fe0b3dcb95e0a996bcd51b5963e49176eb
Ravin Singh D <ravinsinghd@outlook.com>
2020-09-14 12:59:41 +0530

    Added print page

--- app/src/app/billing/print/print.component.ts
+++ app/src/app/billing/print/print.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { map, switchMap } from "rxjs/operators";
+import { HttpService } from "src/app/shared/http.service";
+import { Bill } from "src/app/type";
 
 @Component({
   selector: "app-print",
@@ -6,7 +10,28 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./print.component.scss"],
 })
 export class PrintComponent implements OnInit {
-  constructor() {}
+  currentBill: Bill | null = null;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpService: HttpService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.activatedRoute.paramMap
+      .pipe(
+        map((params) => {
+          const billId = params.get("billId");
+          return billId;
+        })
+      )
+      .subscribe((billId) => this.getBillDetails(billId));
+  }
+
+  getBillDetails(billId: string | null) {
+    this.httpService
+      .getRequest<Bill>(`bills/${billId}`)
+      .subscribe((bill: Bill) => {
+        this.currentBill = bill;
+      });
+  }
 }
