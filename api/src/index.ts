de9db0c42259ac89e141f0d9d02827a5a749f02f
Ravin Singh D <ravinsinghd@outlook.com>
2020-09-19 12:46:17 +0530

    Added public folder

--- api/src/index.ts
+++ api/src/index.ts
@@ -13,6 +13,7 @@ const port = 8081;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'))
 
 app.use('/products', productsModule);
 app.use('/stocks', stockModule);
